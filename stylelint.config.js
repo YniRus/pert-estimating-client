5754999e22881122ff7bb51e45ce334fc91f5c44
YniRus <YniRus@yandex.ru>
2024-07-27 12:16:25 +0300

    Add stylelint and stylelint-order

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+export default {
+    extends: [
+        'stylelint-config-standard-scss',
+        'stylelint-config-standard-vue/scss',
+        '@stylistic/stylelint-config',
+        'stylelint-config-clean-order',
+    ],
+    rules: {
+        '@stylistic/indentation': 4,
+        '@stylistic/string-quotes': 'single',
+    },
+}
