dd8d9d7e02ee95dc1a627f943562cc4af1f1797f
YniRus <YniRus@yandex.ru>
2025-04-15 22:08:43 +0300

    Got http error as data object

--- src/plugins/ofetch.ts
+++ src/plugins/ofetch.ts
@@ -1,9 +1,17 @@
 import {
     type FetchError,
+    type IFetchError,
     type FetchOptions,
     type ResponseType,
     ofetch,
 } from 'ofetch'
+
+declare module 'ofetch' {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    export interface FetchError<T = any, E = any> extends Omit<IFetchError<T>, 'data'> {
+        data: E
+    }
+}
 
 export enum HttpRequestMethod {
     GET = 'GET',
@@ -20,31 +28,39 @@ const baseFetch = ofetch.create({
     credentials: 'include',
 })
 
+export interface FetchErrorData {
+    error: {
+        message: string
+        type: string
+        code?: number
+    }
+}
+
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-async function _get<T = any, R extends ResponseType = 'json'>(path: string, query?: FetchOptions['query'], options?: FetchOptions<R>) {
+async function _get<T = any, E = any, R extends ResponseType = 'json'>(path: string, query?: FetchOptions['query'], options?: FetchOptions<R>) {
     return await baseFetch<T, R>(path, {
         method: HttpRequestMethod.GET,
         query,
         ...options,
-    }).catch((error: FetchError<T>) => error)
+    }).catch((error: FetchError<T, E>) => error)
 }
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-async function _post<T = any, R extends ResponseType = 'json'>(path: string, data?: FetchOptions['body'], options?: FetchOptions<R>) {
+async function _post<T = any, E = any, R extends ResponseType = 'json'>(path: string, data?: FetchOptions['body'], options?: FetchOptions<R>) {
     return await baseFetch<T, R>(path, {
         method: HttpRequestMethod.POST,
         body: data,
         ...options,
-    }).catch((error: FetchError<T>) => error)
+    }).catch((error: FetchError<T, E>) => error)
 }
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-async function _delete<T = any, R extends ResponseType = 'json'>(path: string, query?: FetchOptions['query'], options?: FetchOptions<R>) {
+async function _delete<T = any, E = any, R extends ResponseType = 'json'>(path: string, query?: FetchOptions['query'], options?: FetchOptions<R>) {
     return await baseFetch<T, R>(path, {
         method: HttpRequestMethod.DELETE,
         query,
         ...options,
-    }).catch((error: FetchError<T>) => error)
+    }).catch((error: FetchError<T, E>) => error)
 }
 
 export const request = {
