f27f1a6af683f04bbb93e9d25ad38015e68a22c2
YniRus <YniRus@yandex.ru>
2025-05-04 23:38:04 +0300

    Comment .env

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_SERVER_HOST: string
-    readonly VITE_SERVER_PING_INTERVAL: string
+    readonly VITE_SERVER_HOST?: string
+    readonly VITE_SERVER_PING_INTERVAL?: string
     readonly VITE_APP_TITLE: string
 }
 
